4c664cf3df8b81622e3476f142ba276300c5f4c6
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-18 19:29:06 +0200

    creato pagina admin.blade.php e completato la crud index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="dashboard p-5">
---
+++ resources/views/admin/projects/project.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container p-5">
+    <h1>Vi Lovvo</h1>
+</div>
+@endsection

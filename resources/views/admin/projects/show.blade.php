b47b8fa706ef9d54f279cdffed34a7c408bf4ac0
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-19 12:03:57 +0200

    destroy completato, esercizio con bonus finito

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,7 +10,11 @@
             <img class="w-100" src="{{$project->image}}" alt="">
         
             <div class="py-5">
-                    <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-warning">Modifica</a>     
+                    <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-warning">Modifica</a>
+                    
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Elimina
+                    </button>
             </div>
         </div>
         <div class="col-6">
@@ -22,6 +26,36 @@
             <hr>
             <p><strong>Link: </strong>{{$project->repo_links}}</p>
         </div>  
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina il progetto</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    Sei sicuro che vuoi eliminare il progetto? "{{$project->name}}"
+                </div>
+
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+
+                        <button class="btn btn-danger">Elimina</button>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
 @endsection

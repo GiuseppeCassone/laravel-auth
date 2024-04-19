15fe59c926e54aad63a1bc000fca597a883115e7
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-19 09:20:38 +0200

    CRUD create inserita

--- resources/views/admin/projects/project.blade.php
+++ resources/views/admin/projects/project.blade.php
@@ -3,5 +3,7 @@
 @section('content')
 <div class="container p-5">
     <h1>Pagina project</h1>
+
+    <a href="{{route('admin.projects.create')}}" class="btn btn-info">Crea nuovo progetto</a>
 </div>
 @endsection

cda3fee7ecf87dafe0c225c6303049f7567da2fa
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-20 18:13:49 +0200

    cambiato titolo nella home admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="dashboard p-5">
-    <h1>Pagina Admin</h1>
+    <h1 class="mb-5">Benvenuto {{Auth::user()->name}}</h1>
 
-    <a href="{{route('admin.projects.index')}}" class="btn btn-info">Visualizza i progetti</a>
+    <a href="{{route('admin.projects.index')}}" class="btn btn-info">Visualizza i tuoi progetti</a>
 </div>
 @endsection
 
